ea3d0aaf82dd86c6199ddbbfecf467ca73a86227
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-04-24 00:01:53 +0200

    Add folder[path] to template data as well

--- client/ayon_core/pipeline/template_data.py
+++ client/ayon_core/pipeline/template_data.py
@@ -103,6 +103,7 @@ def get_folder_template_data(folder_entity, project_name):
     return {
         "folder": {
             "name": folder_name,
+            "path": path,
             "type": folder_entity["folderType"],
         },
         "assets_or_shots": "shots" if folder_entity["folderType"] != "Asset" else "assets",
