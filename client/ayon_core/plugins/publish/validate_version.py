b5da486994477cf673852112e97ca509f022084d
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-06-05 18:36:40 +0200

    Revert "Fix validate version to be like before without needing to set settings"
    This reverts commit 4ccdf68735d9753f2c63de67775e49cf7c921eff.

--- client/ayon_core/plugins/publish/validate_version.py
+++ client/ayon_core/plugins/publish/validate_version.py
@@ -1,12 +1,14 @@
 import pyblish.api
-from ayon_core.pipeline import (
+
+from ayon_core.lib import filter_profiles
+from ayon_core.pipeline.publish import (
+    PublishValidationError,
     OptionalPyblishPluginMixin
 )
-from ayon_core.pipeline.publish import PublishValidationError
+from ayon_core.pipeline import get_current_host_name
 
 
-class ValidateVersion(pyblish.api.InstancePlugin,
-                      OptionalPyblishPluginMixin):
+class ValidateVersion(pyblish.api.InstancePlugin, OptionalPyblishPluginMixin):
     """Validate instance version.
 
     AYON does not allow overwriting previously published versions.
@@ -15,14 +17,36 @@ class ValidateVersion(pyblish.api.InstancePlugin,
     order = pyblish.api.ValidatorOrder
 
     label = "Validate Version"
-    hosts = ["nuke", "maya", "houdini", "blender",
-             "photoshop", "aftereffects"]
 
     optional = True
     active = True
 
-    def process(self, instance):
+    @classmethod
+    def apply_settings(cls, settings):
+        # Disable if no profile is found for the current host
+        profiles = (
+            settings
+            ["core"]
+            ["publish"]
+            ["ValidateVersion"]
+            ["plugin_state_profiles"]
+        )
+        profile = filter_profiles(
+            profiles, {"host_names": get_current_host_name()}
+        )
+        if not profile:
+            cls.enabled = False
+            return
 
+        # Apply settings from profile
+        for attr_name in {
+            "enabled",
+            "optional",
+            "active",
+        }:
+            setattr(cls, attr_name, profile[attr_name])
+
+    def process(self, instance):
         if not self.is_active(instance.data):
             return
 
@@ -43,16 +67,16 @@ class ValidateVersion(pyblish.api.InstancePlugin,
                 "Version '{0}' from instance '{1}' that you are "
                 "trying to publish is lower or equal to an existing version "
                 "in the database. Version in database: '{2}'."
-                "Please version up the file to a higher version number "
-                "than: '{2}' or disable the 'Validate version' on the instance."
+                "Please version up your workfile to a higher version number "
+                "than: '{2}'."
             ).format(version, instance.data["name"], latest_version)
 
             msg_html = (
                 "Version <b>{0}</b> from instance <b>{1}</b> that you are "
                 "trying to publish is lower or equal to an existing version "
                 "in the database. Version in database: <b>{2}</b>.<br><br>"
-                "Please version up the file to a higher version number "
-                "than: <b>{2}</b> or disable the 'Validate version' on the instance."
+                "Please version up your workfile to a higher version number "
+                "than: <b>{2}</b>."
             ).format(version, instance.data["name"], latest_version)
             raise PublishValidationError(
                 title="Higher version of publish already exists",
